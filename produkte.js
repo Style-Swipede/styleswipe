209ba195e8afdc0d0270d468d40cb659b650decb
StyleSwipede <info@styleswipe.de>
2025-06-24 19:46:01 +0200

    Produkte-Datei hinzugefügt

---
+++ produkte.js
@@ -0,0 +1,362 @@
+const produkte = [
+  {
+    name: "Beiges Top",
+    kategorie: "oberteil",
+    farbe: "beige",
+    groesse: "xs",
+    anlass: "freizeit",
+    bild: "Top.png",
+    link: "https://h&m.de/"
+  },
+  {
+    name: "Rosa Kleid",
+    kategorie: "kleid",
+    farbe: "rosa",
+    groesse: "xs",
+    anlass: "date",
+    bild: "Kleid.png",
+    link: "https://zara.de/"
+  },
+  {
+    name: "Blaue Jeans",
+    kategorie: "hose",
+    farbe: "blau",
+    groesse: "xs",
+    anlass: "business",
+    bild: "Jeans.png",
+    link: "https://aboutyou.de/"
+  },
+  {
+    name: "Schwarze High Heels",
+    kategorie: "schuhe",
+    farbe: "schwarz",
+    groesse: "37",
+    anlass: "party",
+    bild: "HighHeels.png",
+    link: "https://only.de/"
+  }
+    {
+    name: "Satin Rock",
+    kategorie: "Rock",
+    farbe: "beige",
+    groesse: "xs",
+    anlass: "date",
+    bild: "Skirt.png",
+    link: "https://bershka.de/"
+  }
+    {
+    name: "Beiger Pullover",
+    kategorie: "oberteil",
+    farbe: "beige",
+    groesse: "xs",
+    anlass: "freizeit",
+    bild: "Pulli.png",
+    link: "https://asos.de/"
+  }
+ {
+    name: "Bunter Cardigan",
+    kategorie: "oberteil",
+    farbe: "bunt",
+    groesse: "xs",
+    anlass: "freizeit",
+    bild: "Cardigan.png",
+    link: "https://asos.de/"
+  }
+   {
+    name: "Schwarze Jacke",
+    kategorie: "oberteil",
+    farbe: "schwarz",
+    groesse: "xs",
+    anlass: "freizeit",
+    bild: "Jacke.png",
+    link: "https://only.de/"
+  }
+    {
+    name: "Beiges Top",
+    kategorie: "oberteil",
+    farbe: "beige",
+    groesse: "s",
+    anlass: "freizeit",
+    bild: "Top.png",
+    link: "https://h&m.de/"
+  },
+  {
+    name: "Rosa Kleid",
+    kategorie: "kleid",
+    farbe: "rosa",
+    groesse: "s",
+    anlass: "date",
+    bild: "Kleid.png",
+    link: "https://zara.de/"
+  },
+  {
+    name: "Blaue Jeans",
+    kategorie: "hose",
+    farbe: "blau",
+    groesse: "s",
+    anlass: "business",
+    bild: "Jeans.png",
+    link: "https://aboutyou.de/"
+  },
+  {
+    name: "Schwarze High Heels",
+    kategorie: "schuhe",
+    farbe: "schwarz",
+    groesse: "38",
+    anlass: "party",
+    bild: "HighHeels.png",
+    link: "https://only.de/"
+  }
+    {
+    name: "Satin Rock",
+    kategorie: "Rock",
+    farbe: "beige",
+    groesse: "s",
+    anlass: "date",
+    bild: "Skirt.png",
+    link: "https://bershka.de/"
+  }
+    {
+    name: "Beiger Pullover",
+    kategorie: "oberteil",
+    farbe: "beige",
+    groesse: "s",
+    anlass: "freizeit",
+    bild: "Pulli.png",
+    link: "https://asos.de/"
+  }
+ {
+    name: "Bunter Cardigan",
+    kategorie: "oberteil",
+    farbe: "bunt",
+    groesse: "s",
+    anlass: "freizeit",
+    bild: "Cardigan.png",
+    link: "https://asos.de/"
+  }
+   {
+    name: "Schwarze Jacke",
+    kategorie: "oberteil",
+    farbe: "schwarz",
+    groesse: "s",
+    anlass: "freizeit",
+    bild: "Jacke.png",
+    link: "https://only.de/"
+  }
+  {
+    name: "Beiges Top",
+    kategorie: "oberteil",
+    farbe: "beige",
+    groesse: "m",
+    anlass: "freizeit",
+    bild: "Top.png",
+    link: "https://h&m.de/"
+  },
+  {
+    name: "Rosa Kleid",
+    kategorie: "kleid",
+    farbe: "rosa",
+    groesse: "m",
+    anlass: "date",
+    bild: "Kleid.png",
+    link: "https://zara.de/"
+  },
+  {
+    name: "Blaue Jeans",
+    kategorie: "hose",
+    farbe: "blau",
+    groesse: "m",
+    anlass: "business",
+    bild: "Jeans.png",
+    link: "https://aboutyou.de/"
+  },
+  {
+    name: "Schwarze High Heels",
+    kategorie: "schuhe",
+    farbe: "schwarz",
+    groesse: "39",
+    anlass: "party",
+    bild: "HighHeels.png",
+    link: "https://only.de/"
+  }
+    {
+    name: "Satin Rock",
+    kategorie: "Rock",
+    farbe: "beige",
+    groesse: "m",
+    anlass: "date",
+    bild: "Skirt.png",
+    link: "https://bershka.de/"
+  }
+    {
+    name: "Beiger Pullover",
+    kategorie: "oberteil",
+    farbe: "beige",
+    groesse: "m",
+    anlass: "freizeit",
+    bild: "Pulli.png",
+    link: "https://asos.de/"
+  }
+ {
+    name: "Bunter Cardigan",
+    kategorie: "oberteil",
+    farbe: "bunt",
+    groesse: "m",
+    anlass: "freizeit",
+    bild: "Cardigan.png",
+    link: "https://asos.de/"
+  }
+   {
+    name: "Schwarze Jacke",
+    kategorie: "oberteil",
+    farbe: "schwarz",
+    groesse: "m",
+    anlass: "freizeit",
+    bild: "Jacke.png",
+    link: "https://only.de/"
+  }
+    {
+    name: "Beiges Top",
+    kategorie: "oberteil",
+    farbe: "beige",
+    groesse: "l",
+    anlass: "freizeit",
+    bild: "Top.png",
+    link: "https://h&m.de/"
+  },
+  {
+    name: "Rosa Kleid",
+    kategorie: "kleid",
+    farbe: "rosa",
+    groesse: "l",
+    anlass: "date",
+    bild: "Kleid.png",
+    link: "https://zara.de/"
+  },
+  {
+    name: "Blaue Jeans",
+    kategorie: "hose",
+    farbe: "blau",
+    groesse: "l",
+    anlass: "business",
+    bild: "Jeans.png",
+    link: "https://aboutyou.de/"
+  },
+  {
+    name: "Schwarze High Heels",
+    kategorie: "schuhe",
+    farbe: "schwarz",
+    groesse: "40",
+    anlass: "party",
+    bild: "HighHeels.png",
+    link: "https://only.de/"
+  }
+    {
+    name: "Satin Rock",
+    kategorie: "Rock",
+    farbe: "beige",
+    groesse: "l",
+    anlass: "date",
+    bild: "Skirt.png",
+    link: "https://bershka.de/"
+  }
+    {
+    name: "Beiger Pullover",
+    kategorie: "oberteil",
+    farbe: "beige",
+    groesse: "l",
+    anlass: "freizeit",
+    bild: "Pulli.png",
+    link: "https://asos.de/"
+  }
+ {
+    name: "Bunter Cardigan",
+    kategorie: "oberteil",
+    farbe: "bunt",
+    groesse: "l",
+    anlass: "freizeit",
+    bild: "Cardigan.png",
+    link: "https://asos.de/"
+  }
+   {
+    name: "Schwarze Jacke",
+    kategorie: "oberteil",
+    farbe: "schwarz",
+    groesse: "l",
+    anlass: "freizeit",
+    bild: "Jacke.png",
+    link: "https://only.de/"
+  }
+    {
+    name: "Beiges Top",
+    kategorie: "oberteil",
+    farbe: "beige",
+    groesse: "xl",
+    anlass: "freizeit",
+    bild: "Top.png",
+    link: "https://h&m.de/"
+  },
+  {
+    name: "Rosa Kleid",
+    kategorie: "kleid",
+    farbe: "rosa",
+    groesse: "xl",
+    anlass: "date",
+    bild: "Kleid.png",
+    link: "https://zara.de/"
+  },
+  {
+    name: "Blaue Jeans",
+    kategorie: "hose",
+    farbe: "blau",
+    groesse: "xl",
+    anlass: "business",
+    bild: "Jeans.png",
+    link: "https://aboutyou.de/"
+  },
+  {
+    name: "Schwarze High Heels",
+    kategorie: "schuhe",
+    farbe: "schwarz",
+    groesse: "41",
+    anlass: "party",
+    bild: "HighHeels.png",
+    link: "https://only.de/"
+  }
+    {
+    name: "Satin Rock",
+    kategorie: "Rock",
+    farbe: "beige",
+    groesse: "xl",
+    anlass: "date",
+    bild: "Skirt.png",
+    link: "https://bershka.de/"
+  }
+    {
+    name: "Beiger Pullover",
+    kategorie: "oberteil",
+    farbe: "beige",
+    groesse: "xl",
+    anlass: "freizeit",
+    bild: "Pulli.png",
+    link: "https://asos.de/"
+  }
+ {
+    name: "Bunter Cardigan",
+    kategorie: "oberteil",
+    farbe: "bunt",
+    groesse: "xl",
+    anlass: "freizeit",
+    bild: "Cardigan.png",
+    link: "https://asos.de/"
+  }
+   {
+    name: "Schwarze Jacke",
+    kategorie: "oberteil",
+    farbe: "schwarz",
+    groesse: "xl",
+    anlass: "freizeit",
+    bild: "Jacke.png",
+    link: "https://only.de/"
+  }
+];
